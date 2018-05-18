cdfbaf9254f5ffaa5d8488f108174a1fdc8c7645
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-18 17:50:54 +0530

    cron will run the production server.

--- cron.sh
+++ cron.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-make download
-make 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+(
+    cd $SCRIPT_DIR
+    make download
+    make 
+    ./venv/bin/python serve.py --prod || echo "Already running"
+)
